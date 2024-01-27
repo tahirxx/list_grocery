cbc3906b895ea83725c0fbe29bce2f794dd677bb
tahirxx <59288568+tahirxx@users.noreply.github.com>
2024-01-26 23:31:54 -0500

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,49 @@
 import { useState } from 'react'
 import './App.css'
 import List from './List.jsx';
+import Alert from './Alert.jsx';
+
 
 function App() {
 const [name, setName] = useState('');
 const [list, setList] = useState([]);
 const [isEditing, setIsEditing] = useState(false);
 const [editID, setEditID] = useState(null);
-const [alert, setAlert] = useState({show:false,msg:'',type:''});
+const [alert, setAlert] = useState({show: true, msg:'',type:''});
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+if(!name){
+  //display alert
+}
+else if(name && isEditing) {
+  //deal with edit
+} else {
+  //show alert
+  const newItem = {id: new Date().getTime().toString(), title:name};
+  setList([...list, newItem]);
+  setName('');
+}
 
   return (
     <section className='form-container'>
-      <div className='grocery-container'></div>
-      <form action="">
-        <h2>Grocery Bud</h2>
-        <input className="input" type="text"  />
-        <button className='input-btn'>Add item</button>
-      </form>
+      <form className="" action="" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+              <h2>Grocery Bud</h2>
+              <div className='form-control'>
+              <input className="input" type="text" placeholder='e.g. egg' value={name} onChange={(e) => setName(e.target.value)}/>
+              <button type="submit" className='input-btn'>
+                {isEditing ? 'edit' : 'submit' }Add item</button>
+              </div>
+            </form>
+
+      <div className='grocery-container'>
+        <List items={list}/>
+        
+
+        </div>
+
+     
     </section>
   )
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { FaEdit, FaTrash} from 'react-icon/fa';
 
-export default function List() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+export default function List({ items }) {
+    return (
+        <div className='grocery-list'>
+            {items.map((item) => {
+                const { id, title } = item;
+                return <article key={id} className="grocery-item">
+                    <p className='title'>
+                        {title}
+                    </p>
+                    <div className='btn-container'>
+                        <button type="button" className='edit-btn'>
+                            <FaEdit />
+                        </button>
+
+                        <button type="button" className='delete-btn'>
+                            <FaTrash />
+                        </button>
+
+                    </div>
+
+                </article>
+            })}
+        </div>
+    )
 }
